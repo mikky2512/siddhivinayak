422f8f1d112aecaf8916c28fb8dbbfda46afe8fd
daksh2304 <devanshimistry405@gmail.com>
2025-11-25 18:11:46 +0530

    25/11 D2

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -20,9 +20,9 @@ function Navbar() {
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-          <li><Link to="/services">Services</Link></li>
           <li><Link to="/blogs">Blogs</Link></li>
+          <li><Link to="/services">Services</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
         </ul>
       </nav>
     </header>
