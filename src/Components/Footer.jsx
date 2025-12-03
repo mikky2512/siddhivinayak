3d030a37922583c2139466dea9d2809608a1e2aa
mikky2512 <mahi.n.patel14360@gmail.com>
2025-12-03 13:33:38 +0530

    FINAL COMMIT

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -98,10 +98,11 @@ const Footer = () => {
             <h3>Quick Links</h3>
             <ul>
               <li><a href="/">Home</a></li>
-              <li><a href="/about">About Us</a></li>
+              <li><a href="/about">About</a></li>
+              <li><a href="/blogs">Blogs</a></li>
               <li><a href="/services">Services</a></li>
               <li><a href="/contact">Contact</a></li>
-              <li><a href="/blogs">Blogs</a></li>
+              
             </ul>
           </div>
 
